cd34b50ff8855b04fe9ad120882c4fb1674959c6
Aryan Mishra <mishra2001aryan@gmail.com>
2022-08-17 15:27:25 +0530

    Basic app

---
+++ trello/src/Components/Board/Board.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Board.css";
+function Board() {
+  return (
+    <div className="board">
+      <div className="board_top">
+        <p className="board_top_title">To Do</p>
+        <span>2</span>
+      </div>
+    </div>
+  );
+}
+export default Board;
